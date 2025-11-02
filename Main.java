b81abcdeedc45d332b6c2717280caa9a6a11a637
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-02 19:43:16 +0530

    learn passsing Inputs

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args){
-        System.out.println("Chala JA BSDK");
+        Scanner input = new Scanner(System.in);  
+        System.out.println(input.nextInt());     
     }
 }
