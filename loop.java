77d503159568c1349c46e9cf30445bb9596785e8
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-03 19:28:38 +0530

    today 1st commit

---
+++ loop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter num: ");
+        int num = scanner.nextInt();
+
+        // if (salary > 10000) {
+        //     System.out.println("well done");
+        // } else {
+        //     System.out.println("Work hard bsdk");
+        // }
+        // scanner.close();
+
+    }
+
+}
