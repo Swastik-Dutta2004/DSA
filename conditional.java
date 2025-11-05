4751dec669aba0cb11f31b77afa2cbedabea641b
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-05 19:20:12 +0530

    today 1st commit

--- conditional.java
+++ conditional.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 public class conditional {
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
-        System.out.println("Enter Those numbers: ");
+        // Scanner in = new Scanner (System.in);
+        // System.out.println("Enter Those numbers: ");
+        // int a = in.nextInt();
+        // int b = in.nextInt();
+        // int c = in.nextInt();
+
+        // int max = Math.max(c,  Math.max(a, b));
+
+        // System.out.println(max);
+
+        Scanner in = new Scanner(System.in);
         int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
-
-        int max = Math.max(c,  Math.max(a, b));
-
-        System.out.println(max);
-    }
-    int a, b, c;
+        int b = 1;
+        
     
 }
---
+++ count.java
@@ -0,0 +1,18 @@
+// public class count {
+//     public static void main(String[] args) {
+//         int a = 1234256252;
+//         int count = 0;
+
+//         while (a > 0) {
+//             int temp = a % 10;
+//             if (temp == 2) {
+//                 count++;
+//             }
+//             a /= 10;
+//         }
+//         System.out.println(count);
+//     }
+
+// }
+
+public class 
