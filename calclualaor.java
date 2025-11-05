100aa44c3487ca4227ba8f70d0c4f5cd71d88ec2
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-05 20:05:02 +0530

    5th commit

---
+++ calclualaor.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class calclualaor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+        while (true) {
+            System.out.println("Enter The Operator");
+            char op = sc.next().trim().charAt(0);
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.println("Enter the numbers");
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
+
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    ans = num1 / num2;
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+                else if () {
+                    break;
+                }
+                else{
+                    System.out.println("Invalid operator");
+                }
+            } System.out.println(ans);
+        }
+    }
+
+}
