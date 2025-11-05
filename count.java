34afc8c87bf53ee0f1c7164b59811287743cb2fe
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-05 19:28:50 +0530

    3rd commit

--- count.java
+++ count.java
@@ -17,10 +17,11 @@
 
 public class count {
     public static void main(String[] args) {
+        int digit = 0;
         int a = 1234;
         while (a > 0) {
             int temp = a % 10;
-            int digit = temp * 10;  
+            digit = temp * 10;  
         } 
         a /= 10;
         System.out.println(digit);
