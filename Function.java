e0741d37be256d9008c874ae38cfecf348d31a42
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-08 17:33:08 +0530

    Add marks function and improve formatting in Function class

--- Function.java
+++ Function.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+
 public class Function {
     public static void main(String[] args) {
         sum();
         greeting();
         int result = sum(5, 10);
         System.out.println("The sum from parameters is: " + result);
+        marks();
     }
 
     static void sum() {
@@ -14,19 +16,33 @@ public class Function {
         System.out.print("Enter b number: ");
         int b = sc.nextInt();
         int sum = a + b;
-        System.out.println("The sum is: "+ sum);
+        System.out.println("The sum is: " + sum);
     }
 
     static void greeting() {
         System.out.println("Hello! Welcome to our program.");
     }
 
-    static int sum(int a, int b){
-        return a+ b;
+    static int sum(int a, int b) {
+        return a + b;
 
     }
 
-    static void 
+    static void marks() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your numbre: ");
+        int num = sc.nextInt();
+        if (num > 95) {
+            System.out.println("Excellent");
+        } else if (num > 85) {
+            System.out.println("Very Good");
+        } else if (num > 70) {
+            System.out.println("Good");
+        } else if (num > 50) {
+            System.out.println("Average");
+        } else {
+            System.out.println("Fail");
+        }
 
-
+    }
 }
