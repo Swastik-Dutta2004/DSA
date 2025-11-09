37eeadba3bd955b4f19dd98b17c98608ab3d8119
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-09 12:58:46 +0530

    Implement NestedSwitch functionality for employee ID and department selection

---
+++ lecture10/NestedSwitch.java
@@ -0,0 +1,39 @@
+package lecture10;
+
+import java.util.Scanner;
+
+public class NestedSwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the empID:");
+        int emp = sc.nextInt();
+        String dep = sc.next();
+
+        switch (emp) {
+            case 1:
+                System.out.print("Swastik Dutta");
+                break;
+            case 2:
+                System.out.print("Subhraneel goswami");
+                break;
+            case 3:
+                switch (dep) {
+                    case "It":
+                        System.out.print("It deparment");
+                        break;
+                    case "Manag":
+                        System.out.print("Managment");
+                        break;
+                
+                    default:
+                    System.out.println("Not valid deparment");
+                        break;
+                }
+                break;
+        
+            default:
+                System.out.println("Enter a valid empID");
+                break;
+        }
+    }
+}
