c96443cd4b5a06f0c470b78db81e8d842f7b0ebf
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-09 18:56:17 +0530

    Fix input prompts and data types in Array class for number and name entry

--- lecture12/Array.java
+++ lecture12/Array.java
@@ -6,11 +6,17 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the names: ");
-        String arr [] = new String[4];
+        System.out.print("Enter the numbers: ");
+        int arr [] = new int[4];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.next();
-            System.out.println(arr);
+            arr[i] = sc.nextInt();
+            System.out.println(arr[i] + "");
+        }
+        System.out.print("Enter the names: ");
+        String name [] = new String[4];
+        for (int i = 0; i < name.length; i++) {
+            name[i] = sc.next();
+            System.out.println(name[i]);
         }
     }
 
