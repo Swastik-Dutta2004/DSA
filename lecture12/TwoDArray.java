c1f127f25834ff26fe1efafc6c76f27d8912beb3
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-10 18:27:17 +0530

    Refactor TwoDArray class for improved input handling and output formatting

--- lecture12/TwoDArray.java
+++ lecture12/TwoDArray.java
@@ -8,11 +8,17 @@ public class TwoDArray {
         Scanner sc = new Scanner(System.in);
         int arr [][] = new int[3][4];
 
-        System.out.println("Enter 12 numbers: ");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
+        System.out.println("Enter 12 numbers:");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
                 arr[i][j] = sc.nextInt();
             }
+        }
+
+        System.out.println("Printing 2d Array:");
+        for (int [] a : arr) {
+            System.out.println(Arrays.toString(a));
         }
     }
 }
