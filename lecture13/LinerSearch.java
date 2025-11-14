a6b3729f4ed0c4855d25a458007abe34b41e127f
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-14 18:27:37 +0530

    Refactor LinerSearch class to return index of target and handle empty array case

--- lecture13/LinerSearch.java
+++ lecture13/LinerSearch.java
@@ -4,18 +4,21 @@ public class LinerSearch {
     public static void main(String[] args) {
         int [] nums = {1, 3,5,7,63,23,89};
         int target = 63;
-        liner(nums, target);
+        int ans = liner(nums, target);
+        System.out.println(ans);
     }
 
-    static void liner(int [] arr, int target){
+       static int liner(int[] arr, int target) {
         if (arr.length == 0) {
-            System.out.println("Enter somethink into the array");
+            return -1; // empty array
         }
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
+            int element = arr[i];
+            if (element == target) {
                 System.out.println("Found the target in index number: " + i);
             }
         }
+        return -1 ;
     }
 }
