0fc0ce6bb97bfd387bea56cf49d450eccf1d4d7c
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-22 20:06:42 +0530

    Refactor LinerSearch1 class to remove main method and isolate element input functionality

--- Practice/LinerSearch1.java
+++ Practice/LinerSearch1.java
@@ -3,29 +3,14 @@ package Practice;
 import java.util.Scanner;
 
 public class LinerSearch1 {
-   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int count = 0;
-
-    int [] arr = new int[5];
-    System.out.println("Enter 5 numbers: ");
-    for (int i = 0; i < arr.length; i++) {
-        arr[i] = sc.nextInt();
-    }
-
-    System.out.println("Enter the number you wnat to find: ");
-    int target = sc.nextInt();
-
-    for (int i = 0; i < arr.length; i++) {
-        if (arr[i] == target) {
-            count ++ ;
+    static Scanner sc = new Scanner(System.in);
+    static void elementFound(){
+        int arr [] = new int[5];
+        System.out.println("Enter 5 numbers: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-        if (count > 0) {
-            System.out.println("Your enter number has appeared" + count + "times");
-        }else {
-            System.out.println("Didn't found");
-        }
+
+        
     }
-   }
-   
 }
