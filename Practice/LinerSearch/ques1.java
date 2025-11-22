cc4b22cf394c98fa4ae863c7c68680ab8e97dc1a
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-22 12:53:07 +0530

    Refactor TwoDArray class to improve structure and add main method; create ques1 class for linear search functionality

---
+++ Practice/LinerSearch/ques1.java
@@ -0,0 +1,20 @@
+package Practice.LinerSearch;
+
+import java.util.Scanner;
+
+public class ques1 {
+    static Scanner sc = new Scanner(System.in);
+
+    static void elementSearch(){
+        int []arr = new int[5];
+        System.out.println("Enter 5 Number: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int target;
+        
+        System.err.println("Enter the value you want to found: ");
+        
+    }
+}
--- lecture13/TwoDArray.java
+++ lecture13/TwoDArray.java
@@ -1,9 +1,14 @@
 package lecture13;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TwoDArray {
-Scanner sc = new Scanner(System.in);
+static Scanner sc = new Scanner(System.in);
+
+public static void main(String[] args) {
+    search();
+}
 
     static void search(){
         int [][] arr = new int [3][4];
@@ -12,6 +17,22 @@ Scanner sc = new Scanner(System.in);
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Printing 2d Array:");
+        for (int [] a : arr) {
+            System.out.println(Arrays.toString(a));
+        }
+
+        System.out.println("Enter the number you want to find: ");
+        int target = sc.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (target == arr[i][j]) {
+                    System.out.println("Value found at index: (" + i + ", " + j + ")");
+                }
             }
         }
     }
