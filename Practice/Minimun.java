6507603cfda62cd94a7cf90bdcb5b8f18a659d71
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-26 17:32:14 +0530

    Refactor Minimun class to improve element input and find minimum/maximum functionality

--- Practice/Minimun.java
+++ Practice/Minimun.java
@@ -5,15 +5,38 @@ import java.util.Scanner;
 public class Minimun {
     public static void main(String[] args) {
         int arr[] = minimunElemen();
+        findElement(arr);
     }
     static Scanner sc = new Scanner(System.in);
     static int[] minimunElemen (){
-        System.out.println("Enter 5 elements: ");
+        System.out.print("Enter 5 elements: ");
         int arr[] = new int[5];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         return arr;
     }
-    static void 
+    static void findElement(int arr[]) {
+    int min = arr[0];
+    int max = arr[0];
+
+    int i = 0;
+    int j = arr.length - 1;
+
+    while (i <= j) {
+        // check from the left side
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+
+        // check from the right side
+        if (arr[j] < min) min = arr[j];
+        if (arr[j] > max) max = arr[j];
+
+        i++;
+        j--;
+    }
+
+    System.out.println("Minimum = " + min + " Maximum = " + max);
+}
+
 }
