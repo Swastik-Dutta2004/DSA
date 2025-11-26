e8457db3f0ea5312b20f7990de00c9226f21749d
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-26 16:56:55 +0530

    Add Minimun class to implement input collection for five elements

---
+++ Practice/Minimun.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Minimun {
+    static Scanner sc = new Scanner(System.in);
+    static int[] minimunElemen (){
+        System.out.println("Enter 5 elements: ");
+        int arr[] = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+}
