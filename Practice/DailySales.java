7d70c7fadc7f05f4eba741323e30f9403325dd9a
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-27 17:34:58 +0530

    completed the min, max function.

--- Practice/DailySales.java
+++ Practice/DailySales.java
@@ -3,7 +3,14 @@ package Practice;
 import java.util.Scanner;
 
 public class DailySales {
+    public static void main(String[] args) {
+        int arr[] = userInput();
+        Maximum(arr);
+        Minimun(arr);
+        
+    }
     static Scanner sc = new Scanner(System.in);
+
     static int[] userInput(){
         System.out.println("Enter sales of 5 days: ");
         int arr[] = new int[5];
@@ -13,7 +20,23 @@ public class DailySales {
         return arr;
     }
 
-    static void arrangment(int arr[]){
-        
+    static void Maximum(int arr[]){
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("The maximum sale is: "+ max);
     }
+    static void Minimun(int arr[]){
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        } 
+        System.out.println("The minimun sale is: "+ min);
+    }
+
 }
--- Practice/Minimun.java
+++ Practice/Minimun.java
@@ -7,8 +7,10 @@ public class Minimun {
         int arr[] = minimunElemen();
         findElement(arr);
     }
+
     static Scanner sc = new Scanner(System.in);
-    static int[] minimunElemen (){
+
+    static int[] minimunElemen() {
         System.out.print("Enter 5 elements: ");
         int arr[] = new int[5];
         for (int i = 0; i < arr.length; i++) {
@@ -16,27 +18,32 @@ public class Minimun {
         }
         return arr;
     }
+
     static void findElement(int arr[]) {
-    int min = arr[0];
-    int max = arr[0];
+        int min = arr[0];
+        int max = arr[0];
 
-    int i = 0;
-    int j = arr.length - 1;
+        int i = 0;
+        int j = arr.length - 1;
 
-    while (i <= j) {
-        // check from the left side
-        if (arr[i] < min) min = arr[i];
-        if (arr[i] > max) max = arr[i];
+        while (i <= j) {
+            // check from the left side
+            if (arr[i] < min)
+                min = arr[i];
+            if (arr[i] > max)
+                max = arr[i];
 
-        // check from the right side
-        if (arr[j] < min) min = arr[j];
-        if (arr[j] > max) max = arr[j];
+            // check from the right side
+            if (arr[j] < min)
+                min = arr[j];
+            if (arr[j] > max)
+                max = arr[j];
 
-        i++;
-        j--;
+            i++;
+            j--;
+        }
+
+        System.out.println("Minimum = " + min + " Maximum = " + max);
     }
-
-    System.out.println("Minimum = " + min + " Maximum = " + max);
-}
 
 }
