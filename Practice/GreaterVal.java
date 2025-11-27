9bdc7b2508c1e0ec77ce5b7564c98ce8b32c15b1
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-27 10:17:39 +0530

    Refactor GreaterVal class to use a single Scanner instance and improve code structure

--- Practice/GreaterVal.java
+++ Practice/GreaterVal.java
@@ -3,8 +3,27 @@ package Practice;
 import java.util.Scanner;
 
 public class GreaterVal {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
+        int arr[] = takeEle();
+        FindEle(arr);
+    }
+    static int[] takeEle() {
+        int arr[] = new int[5];
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    static void FindEle(int arr[]){
+        System.out.println("Enter the number: ");
+        int target = sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > target) {
+                System.out.println("Greater than your enter number: " + arr[i]);
+            }
+        }
     }
 }
