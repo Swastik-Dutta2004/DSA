9464226417c2c5e08f3a5eb537e6b7f2036c7221
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-27 08:24:42 +0530

    Add FindChar class to implement character input functionality

---
+++ Practice/FindChar.java
@@ -0,0 +1,18 @@
+package Practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FindChar {
+    static Scanner sc = new Scanner(System.in);
+
+   static ArrayList<String> takingele() {
+    ArrayList<String> list = new ArrayList<>();
+    for (int i = 0; i < list.length; i++) {
+        list = sc.next(null);
+    }
+    return list;
+}
+
+}
