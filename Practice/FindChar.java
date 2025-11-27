d659cb1523238f6db1fec7b1eddbd8ca231c415e
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-27 08:38:30 +0530

    Implement element input functionality in FindChar class and remove LinerSearch class

--- Practice/FindChar.java
+++ Practice/FindChar.java
@@ -6,13 +6,30 @@ import java.util.Scanner;
 
 public class FindChar {
     static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        ArrayList<String> element = takingele();
+    }
 
    static ArrayList<String> takingele() {
     ArrayList<String> list = new ArrayList<>();
-    for (int i = 0; i < list.length; i++) {
-        list = sc.next(null);
+
+    System.out.println("Enter number of elements: ");
+    int n = sc.nextInt();
+    sc.nextLine(); // consume newline
+
+    for (int i = 0; i < n; i++) {
+        System.out.println("Enter element " + (i + 1) + ": ");
+        String value = sc.nextLine();
+        list.add(value);
     }
     return list;
 }
+    static void FindEle (ArrayList<String>list){
+        String target = sc.nextLine();
+        for (int i = 0; i < array.length; i++) {
+            
+        }
+
+    }
 
 }
