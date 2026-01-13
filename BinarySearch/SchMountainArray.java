76db7603faf7f06d6b7fb754be9d48d11c3e1672
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-01-13 20:11:38 +0530

    All porblem are solved.

--- BinarySearch/SchMountainArray.java
+++ BinarySearch/SchMountainArray.java
@@ -4,45 +4,47 @@ import java.util.Scanner;
 
 public class SchMountainArray {
 
-    public static void main(String[] args) {
-        
+     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the number of the element: ");
+        System.out.print("Enter the number of elements: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
 
         System.out.print("Enter the elements: ");
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        System.out.println("Enter the target value you want to find: ");
+        System.out.print("Enter the target value: ");
         int target = sc.nextInt();
 
         int peak = peakElement(arr);
-        int firstPart = OrderAgnostic(arr, target, 0, peak);
 
-        if (firstPart == -1) {
-            System.out.println("Target found at index: " + firstPart);
+        int index = OrderAgnostic(arr, target, 0, peak);
+
+        if (index == -1) {
+            index = OrderAgnostic(arr, target, peak + 1, arr.length - 1);
+        }
+
+        if (index != -1) {
+            System.out.println("Target found at index: " + index);
         } else {
             System.out.println("Target not found");
         }
-
-
     }
 
     static int OrderAgnostic(int[] arr, int target, int start, int end) {
-        
+
         boolean isAsc = arr[start] < arr[end];
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
             if (arr[mid] == target) {
-                System.out.println("Your target element is in " + mid + " number index.");
-                
+                return mid;
             }
             if (isAsc) {
                 if (arr[mid] < target) {
@@ -58,23 +60,22 @@ public class SchMountainArray {
                 }
             }
         }
-        System.out.println("Element not found");
         return -1;
     }
 
-    static int peakElement(int[] arr){
+    static int peakElement(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
 
         while (start < end) {
-            int mid = start + (end - start)/2;
+            int mid = start + (end - start) / 2;
 
             if (arr[mid] > arr[mid + 1]) {
                 end = mid;
-            }else{
+            } else {
                 start = mid + 1;
             }
         }
-        return -1;
+        return start;
     }
 }
