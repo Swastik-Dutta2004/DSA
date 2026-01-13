6d36309f5c0559c65a02f5c419647f60cb5aa6be
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-01-13 11:56:02 +0530

    finding target from Infinite array has completed.

--- BinarySearch/InfiniteArr.java
+++ BinarySearch/InfiniteArr.java
@@ -1,14 +1,58 @@
 package BinarySearch;
 
+import java.util.Scanner;
+
 public class InfiniteArr {
-    static void findtarget(int[] arr, int target) {
-        int start = 0;
-        int end = arr.length - 1;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        while(start <= end){
-            int mid = start + (end - start)/2;
+        System.out.println("Enter the numbers of elements you want: ");
+        int n = sc.nextInt();
 
-            
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
+
+        System.out.println("Enter the target value:");
+        int target = sc.nextInt();
+
+        int index = ans(arr, target);
+
+        if (index != -1) {
+            System.out.println("Target found at index: " + index);
+        } else {
+            System.out.println("Target not found");
+        }
+    }
+
+    static int ans(int[] arr, int target ) {
+        int start = 0;
+        int end = 1;
+
+        while (target > arr[end]){
+            start = end + 1;
+            end = start + (end - start + 1)/2;
+        }
+        return BinarySearch(arr, target, start, end);
+        
+    }
+
+    static int BinarySearch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
