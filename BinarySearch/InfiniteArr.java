32a5cf82546cb72f0ca1b626646eaa874badf387
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-01-13 10:00:34 +0530

    Start the morning with binary search.

---
+++ BinarySearch/InfiniteArr.java
@@ -0,0 +1,14 @@
+package BinarySearch;
+
+public class InfiniteArr {
+    static void findtarget(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            
+        }
+    }
+}
