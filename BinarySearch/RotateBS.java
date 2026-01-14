fc95aaa4a66769dd88fb00ab29097fb0d5d8de77
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-01-14 13:23:52 +0530

    At least i fucked the whole rotated array.

--- BinarySearch/RotateBS.java
+++ BinarySearch/RotateBS.java
@@ -10,32 +10,72 @@ public class RotateBS {
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-        
+
         System.out.print("Enter the elements of the array: ");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
 
-        System.out.print("Enter the target: ");
+        System.out.println("Enter the target value you want to find: ");
         int target = sc.nextInt();
 
-        int pivot = RotatedArray(arr, target);
+        int result = ans(arr, target);
 
-        System.out.println("So the pivot is in index number: " + pivot + "");
+        if (result != -1) {
+            System.out.print("Target found at index: " + result);
+        } else {
+            System.out.print("Target not found");
+        }
     }
-    static int RotatedArray(int[] arr, int target){
-        int start = 0, end = arr.length - 1 ;
+
+    static int ans(int[] arr, int target) {
+        int pivot = RotatedArray(arr);
+
+        if (pivot == -1) {
+            return BinarySch(arr, target, 0, arr.length - 1);
+        }
+        if (arr[pivot] == target) {
+            return pivot;
+        }
+        if (arr[0] > target) {
+            return BinarySch(arr, target, pivot + 1, arr.length - 1);
+        }
+        if (arr[0] < target) {
+            return BinarySch(arr, target, 0, pivot + 1);
+        }
+        return -1;
+    }
+
+    static int BinarySch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                System.out.println("Your searched element has found in the index of :   " + mid);
+
+            }
+            System.out.println("Element not found");
+        }
+        return -1;
+    }
+
+    static int RotatedArray(int[] arr) {
+        int start = 0, end = arr.length - 1;
 
         while (start < end) {
-            int mid = start + (end - start)/2;
+            int mid = start + (end - start) / 2;
 
-            if (arr[mid] > arr[mid + 1]) {
+            if (mid < end && arr[mid] > arr[mid + 1]) {
                 return mid;
-            }else if (arr[mid] < arr[mid - 1]) {
+            } else if (mid > start && arr[mid] < arr[mid - 1]) {
                 return mid - 1;
-            }else if (arr[start] > arr [mid]) {
+            } else if (arr[start] > arr[mid]) {
                 return end = mid - 1;
-            }else if (arr[start] < arr [mid]) {
+            } else if (arr[start] < arr[mid]) {
                 return start = mid + 1;
             }
         }
