83934622e04e05529d6c8b36503f7f951e2febff
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-01-15 13:20:19 +0530

    Done

--- BinarySearch/SplitArray.java
+++ BinarySearch/SplitArray.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class SplitArray {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of the element: ");
@@ -24,17 +23,17 @@ public class SplitArray {
         System.out.println(ans);
     }
 
-    static int SplitArray(int[] arr, int m){
+    static int SplitArray(int[] arr, int m) {
         int start = 0;
         int end = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            start = Math.max(start, arr[i]);
+            start = Math.max(start, i);
             end += arr[i];
         }
 
         while (start < end) {
-            int mid = start + (end - start)/2;
+            int mid = start + (end - start) / 2;
 
             int sum = 0;
             int pices = 1;
@@ -43,18 +42,17 @@ public class SplitArray {
                 if (sum + arr[i] > mid) {
                     sum = arr[i];
                     pices++;
-                }else{
+                } else {
                     sum += arr[i];
                 }
             }
 
-            if (pices > m) {
-                start = mid + 1;
-            }else{
+            if (pices <= m) {
                 end = mid;
+            } else {
+                start = mid + 1;
             }
         }
-
         return end;
     }
 }
