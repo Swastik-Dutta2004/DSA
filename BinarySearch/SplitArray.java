72553f25eaaa5aa0e080d03a5b8c8467b843acb9
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-01-15 11:05:35 +0530

    Spliting array has completed

--- BinarySearch/SplitArray.java
+++ BinarySearch/SplitArray.java
@@ -1,10 +1,30 @@
 package BinarySearch;
 
+import java.util.Scanner;
+
 public class SplitArray {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of the element: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter the elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the number of max splir you want to do: ");
+        int m = sc.nextInt();
+
+        int ans = SplitArray(arr, m);
+        System.out.println(ans);
     }
-    public int SplitArray(int[] arr, int m){
+
+    static int SplitArray(int[] arr, int m){
         int start = 0;
         int end = 0;
 
@@ -14,7 +34,25 @@ public class SplitArray {
         }
 
         while (start < end) {
-            int mid = 
+            int mid = start + (end - start)/2;
+
+            int sum = 0;
+            int pices = 1;
+
+            for (int i = 0; i < arr.length; i++) {
+                if (sum + arr[i] > mid) {
+                    sum = arr[i];
+                    pices++;
+                }else{
+                    sum += arr[i];
+                }
+            }
+
+            if (pices > m) {
+                start = mid + 1;
+            }else{
+                end = mid;
+            }
         }
 
         return end;
