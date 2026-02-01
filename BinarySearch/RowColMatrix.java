c7da1e4373405efec83b0b67bdc6df44f33c99c5
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-02-01 21:44:08 +0530

    Done

--- BinarySearch/RowColMatrix.java
+++ BinarySearch/RowColMatrix.java
@@ -4,27 +4,28 @@ import java.util.Arrays;
 
 public class RowColMatrix {
     public static void main(String[] args) {
-        int[][] arr = {
+        int arr[][] = {
             {10,20,30,40},
             {12,23,33,43},
             {13,24,34,45},
             {15,25,35,45}
         };
-        System.out.println(Arrays.toString(search(arr, 45)));
+        System.out.println(Arrays.toString(Search(arr,45 )));
     }
-    static int[] search(int[][] arr, int target){
+    static int[] Search(int[][] arr, int target) {
         int r = 0;
-        int c = arr.length - 1;
+        int c = arr[0].length - 1;
+
         while (r < arr.length && c >= 0) {
             if (target == arr[r][c]) {
-                return new int[]{r,c};
+                return new int[] { r, c };
             }
             if (target > arr[r][c]) {
-                r ++;
-            }else{
-                c ++;
+                r++;
+            } else {
+                c--;
             }
         }
-        return new int []{-1, -1};
+        return new int[] { -1, -1 };
     }
 }
