5de5b59755c17f2b459e3ece4ca421b6bb18cdfa
Swastik Dutta <duttaswastik004@gmail.com>
2026-02-17 00:30:21 +0530

    Break  the loop if it is sorted.

--- BubbleSort/FirstQu.java
+++ BubbleSort/FirstQu.java
@@ -1,19 +1,25 @@
-import java.util.Arrays;;
+import java.util.Arrays;
 
 public class FirstQu {
     public static void main(String[] args) {
-        int[] arr = {5,4,3,2,1};
+        int[] arr = {1,2,3,4,5};
         BubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void BubbleSort(int[] arr){
+        boolean Swapping;
         for (int i = 0; i < arr.length; i++) {
+            Swapping = false;
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
+                    Swapping = true;
                 }
+            }
+            if (!Swapping) {
+                break;
             }
         }
     }
