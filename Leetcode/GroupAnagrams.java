9417d6c592048dc849bc13d43c8cd2c63f530734
Swastik Dutta <duttaswastik004@gmail.com>
2026-02-21 01:03:09 +0530

    One medium question have done.

--- Leetcode/GroupAnagrams.java
+++ Leetcode/GroupAnagrams.java
@@ -1,16 +1,30 @@
 package Leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class GroupAnagrams {
-    static void Anagrams(String[] strs){
-        for (int i = 0; i < strs.length; i++) {
-            char[] arr = strs[i].toCharArray();
+    public static void main(String[] args) {
+        String[] str = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(Anagram(str));
+    }
+    static List<List<String>> Anagram(String[] str){
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (int i = 0; i < str.length; i++) {
+
+            char[] arr = str[i].toCharArray();
             Arrays.sort(arr);
-            String sorted = new String(arr);
-            for (int j = 0; j < sorted.length(); j++) {
-                
+            String key = new String(arr);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
             }
+
+            map.get(key).add(str[i]);
         }
+        return new ArrayList<>(map.values());
     }
 }
