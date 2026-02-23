c2a51e04a893708154232a898cdec10e24162128
Swastik Dutta <duttaswastik004@gmail.com>
2026-02-23 17:23:07 +0530

    Finding duplicates in an array by sorting

--- Leetcode/DuplicateNum.java
+++ Leetcode/DuplicateNum.java
@@ -5,35 +5,38 @@ import java.util.Arrays;
 
 public class DuplicateNum {
     public static void main(String[] args) {
-            int[] arr = {5,2,3,1,4};
-            CycleSort(arr);
-            System.out.println(Arrays.toString(arr));
-        }
-    static void CycleSort(int[]arr){
+        int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 };
+        ArrayList<Integer> result = findDuplicates(arr);
+        System.out.println(result);
+    }
+
+    static ArrayList<Integer> findDuplicates(int[] arr) {
+
         int i = 0;
-
         while (i < arr.length) {
-            int currectIndex = arr[i] - 1;
+            int correctIndex = arr[i] - 1;
 
-            if (arr[i] != arr[currectIndex]) {
-                Swap(arr, i, currectIndex);
-            }else{
+            if (arr[i] != arr[correctIndex]) {
+                Swap(arr, i, correctIndex);
+            } else {
                 i++;
             }
         }
 
         ArrayList<Integer> ans = new ArrayList<>();
+
         for (int index = 0; index < arr.length; index++) {
-            if (arr[index] != arr[arr[i]]) {
-                 ans.add(arr[index] -1);
-                index++;
+            if (arr[index] != index + 1) {
+                ans.add(arr[index]);
             }
         }
+
+        return ans;
     }
-    
+
     static void Swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
-    }   
+    }
 }
