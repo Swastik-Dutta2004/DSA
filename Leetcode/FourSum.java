e95dce82b8dc7f1623c48020271f8205573e921b
Swastik Dutta <duttaswastik004@gmail.com>
2026-03-14 16:48:55 +0530

    fully completed

--- Leetcode/FourSum.java
+++ Leetcode/FourSum.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 
 public class FourSum {
     public static void main(String[] args) {
-        int[] arr = {1,0,-1,0,-2,2};
-        ArrayList<ArrayList<Integer>> ans = sort(arr, 0);
+        int[] arr = {2,2,2,2,2};
+        ArrayList<ArrayList<Integer>> ans = sort(arr, 8);
         System.out.println(ans);
     }
     static ArrayList<ArrayList<Integer>> sort(int[] arr, int target){
@@ -15,7 +15,11 @@ public class FourSum {
         Arrays.sort(arr);
 
         for (int i = 0; i < arr.length - 3; i++) {
+            if(i > 0 && arr[i] == arr[i-1]) continue;
+
             for (int j = i + 1; j < arr.length - 2; j++) {
+                 if(j > i+1 && arr[j] == arr[j-1]) continue;
+                 
                 int left = j + 1;
                 int right = arr.length - 1;
 
@@ -34,8 +38,8 @@ public class FourSum {
                         temp.add(arr[right]);
 
                         result.add(temp);
-                        left ++;
-                        right --;
+                        while(left < right && arr[left] == arr[left-1]) left++;
+                        while(left < right && arr[right] == arr[right+1]) right--;
                         
                     }
                 }
